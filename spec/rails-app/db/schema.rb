c05520930c6d8bc1dc7bdabb4d663bd8105e390d
Daniël W. Crompton <daniel.crompton@gmail.com>
2014-12-12 14:56:32 +0100

    Changes to make travis run. Tested with: export RAILS_VERSION=3.1.0 ; rm Gemfile.lock ; bundle install && bundle exec rake RAILS_ENV=test export RAILS_VERSION=3.2.0 ; rm Gemfile.lock ; bundle install && bundle exec rake RAILS_ENV=test export RAILS_VERSION=4.0.0 ; rm Gemfile.lock ; bundle install && bundle exec rake RAILS_ENV=test export RAILS_VERSION=4.1.0 ; rm Gemfile.lock ; bundle install && bundle exec rake RAILS_ENV=test

--- spec/rails-app/db/schema.rb
+++ spec/rails-app/db/schema.rb
@@ -9,11 +9,11 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended that you check this file into your version control system.
+# It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141212000731) do
+ActiveRecord::Schema.define(:version => 20141212000731) do
 
-  create_table "avatars", force: true do |t|
+  create_table "avatars", :force => true do |t|
     t.string   "avatar_file_name"
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 20141212000731) do
     t.datetime "updated_at"
   end
 
-  create_table "meta", force: true do |t|
+  create_table "meta", :force => true do |t|
     t.string   "montage_file_name"
     t.string   "montage_content_type"
     t.integer  "montage_file_size"
