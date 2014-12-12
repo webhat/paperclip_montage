0f402db3d7d9c6a15549a658eb6423cd8c6057d8
Daniël W. Crompton <daniel.crompton@gmail.com>
2014-12-12 15:28:04 +0100

    Fixes for spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,37 +21,37 @@ Bundler.setup
 
 require 'paperclip' # and any other gems you need
 require 'paperclip_montage' # and any other gems you need
-require 'rails-app/config/environment'
-require 'rails/all'
-require 'rspec/rails'
+#require 'rails-app/config/environment'
+#require 'rails/all'
+#require 'rspec/rails'
 require "paperclip/matchers"
 
 RSpec.configure do |config|
-  # rspec-expectations config goes here. You can use an alternate
-  # assertion/expectation library such as wrong or the stdlib/minitest
-  # assertions if you prefer.
-  config.expect_with :rspec do |expectations|
-    # This option will default to `true` in RSpec 4. It makes the `description`
-    # and `failure_message` of custom matchers include text for helper methods
-    # defined using `chain`, e.g.:
-    # be_bigger_than(2).and_smaller_than(4).description
-    #   # => "be bigger than 2 and smaller than 4"
-    # ...rather than:
-    #   # => "be bigger than 2"
-    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
-  end
+	# rspec-expectations config goes here. You can use an alternate
+	# assertion/expectation library such as wrong or the stdlib/minitest
+	# assertions if you prefer.
+	config.expect_with :rspec do |expectations|
+		# This option will default to `true` in RSpec 4. It makes the `description`
+		# and `failure_message` of custom matchers include text for helper methods
+		# defined using `chain`, e.g.:
+		# be_bigger_than(2).and_smaller_than(4).description
+		#   # => "be bigger than 2 and smaller than 4"
+		# ...rather than:
+		#   # => "be bigger than 2"
+		#    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+	end
 
-  # rspec-mocks config goes here. You can use an alternate test double
-  # library (such as bogus or mocha) by changing the `mock_with` option here.
-  config.mock_with :rspec do |mocks|
-    # Prevents you from mocking or stubbing a method that does not exist on
-    # a real object. This is generally recommended, and will default to
-    # `true` in RSpec 4.
-    mocks.verify_partial_doubles = true
-  end
+	# rspec-mocks config goes here. You can use an alternate test double
+	# library (such as bogus or mocha) by changing the `mock_with` option here.
+	config.mock_with :rspec do |mocks|
+		# Prevents you from mocking or stubbing a method that does not exist on
+		# a real object. This is generally recommended, and will default to
+		# `true` in RSpec 4.
+		#    mocks.verify_partial_doubles = true
+	end
 
-# The settings below are suggested to provide a good initial experience
-# with RSpec, but feel free to customize to your heart's content.
+	# The settings below are suggested to provide a good initial experience
+	# with RSpec, but feel free to customize to your heart's content.
 	begin
 		# These two settings work together to allow you to limit a spec run
 		# to individual examples or groups you care about by tagging them with
@@ -65,7 +65,7 @@ RSpec.configure do |config|
 		#   - http://myronmars.to/n/dev-blog/2012/06/rspecs-new-expectation-syntax
 		#   - http://teaisaweso.me/blog/2013/05/27/rspecs-new-message-expectation-syntax/
 		#   - http://myronmars.to/n/dev-blog/2014/05/notable-changes-in-rspec-3#new__config_option_to_disable_rspeccore_monkey_patching
-		config.disable_monkey_patching!
+		#config.disable_monkey_patching!
 
 		# This setting enables warnings. It's recommended, but in some cases may
 		# be too noisy due to issues in dependencies.
@@ -78,7 +78,7 @@ RSpec.configure do |config|
 			# Use the documentation formatter for detailed output,
 			# unless a formatter has already been configured
 			# (e.g. via a command-line flag).
-			config.default_formatter = 'doc'
+			#config.default_formatter = 'doc'
 		end
 
 		# Print the 10 slowest examples and example groups at the
